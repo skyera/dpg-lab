037f478aa47c2f2ae44723d3136fd5dbf63e007c
skyera <zgliu2@gmail.com>
2023-02-25 18:53:35 -0800

    refactor

--- test_dear1.py
+++ test_dear1.py
@@ -1,8 +1,10 @@
 import dearpygui.dearpygui as dpg
+import dearpygui.demo as demo
 from faker import Faker
 from mdgen import MarkdownPostProvider
 import multiprocessing
 import platform
+import psutil
 import pyfiglet
 import random
 import subprocess
@@ -37,60 +39,29 @@ def get_checked_tc_count():
 
 
 def get_banner():
-    ascii_banner = pyfiglet.figlet_format('Unit Testing', width=80)
+    ascii_banner = pyfiglet.figlet_format('Test Action', font='slant')
     return ascii_banner
 
 
-def report_mem(mq):
-    for i in range(100):
-        time.sleep(0.1)
-        x = random.random() * 100
-        mq.put((i, x))
-    mq.put('done')
-    print('report_mem: done')
+class ReportMemoryProcess(multiprocessing.Process):
+    def __init__(self, queue, count, interval):
+        super().__init__()
+        self.queue = queue
+        self.count = count
+        self.interval = interval
 
-
-def monitor_mem(mq):
-    datax = []
-    datay = []
-    p  = 0
-    dpg.set_value('progress', p)
-    while True:
-        x = mq.get()
-        if x == 'done':
-            print('done')
-            break
-        datax.append(x[0])
-        datay.append(x[1])
-        dpg.set_value('mem', [datax, datay])
-        p += 0.1
-        dpg.set_value('progress', p)
-
-    x = random.random()
-    print(x)
-    if x > 0.5 :
-        color = (0, 255, 0)
-        text = 'Sucess'
-    else:
-        color = (255, 0, 0)
-        text = 'Failed'
-    dpg.configure_item('run_status', color=color)
-    dpg.set_value('run_status', text)
-    dpg.set_value('test_summary', get_fake_markdown())
-    dpg.set_value('test_xml', get_fake_text())
-    dpg.set_value('test_valgrind', get_fake_text())
-    dpg.configure_item('ind', show=False)
-    dpg.configure_item('run_button', enabled=True)
-
-
-def main():
-    app = App()
-    app.run()
+    def run(self):
+        for i in range(self.count):
+            time.sleep(self.interval)
+            x = random.random() * 100
+            #x = psutil.virtual_memory().total
+            self.queue.put((i, x))
+        self.queue.put('done')
 
 
 class MainWindow:
     def __init__(self):
-        pass
+        self.queue = multiprocessing.Queue()
 
     def create(self):
         datax = []
@@ -134,9 +105,16 @@ class MainWindow:
             with dpg.group(horizontal=True):
                 dpg.add_button(label='Select All', callback=self.select_all_callback)
                 dpg.add_button(label='Select None', callback=self.select_none_callback)
-                dpg.add_button(label='Refresh')
+                dpg.add_button(label='demo', callback=demo.show_demo)
+                dpg.add_button(label='registery', callback=dpg.show_item_registry)
             
             dpg.add_separator()
+            
+            with dpg.group():
+                dpg.add_drag_int(label='count', default_value=100, max_value=1000,tag='count')
+                dpg.add_input_float(label='interval', default_value=0.1, tag='interval')
+            dpg.add_spacer()
+
             with dpg.group(horizontal=True):
                 dpg.add_drag_int(label='Repeat', default_value=1, width=200, min_value=1)
                 dpg.add_button(label='Run', width=100, tag='run_button',
@@ -158,10 +136,12 @@ class MainWindow:
 
             with dpg.tab_bar():
                 with dpg.tab(label='Memory Plot'):
-                    with dpg.plot(label='Memory', height=-1, width=-1):
+                    with dpg.plot(label='Memory', height=-1, width=-1, tag='plot'):
                         dpg.add_plot_legend()
                         dpg.add_plot_axis(dpg.mvXAxis, label='Time(seconds)')
                         dpg.add_plot_axis(dpg.mvYAxis, label='vss', tag='y_axis')
+                        dpg.set_axis_limits_auto('y_axis')
+                        dpg.fit_axis_data('y_axis')
                         dpg.add_line_series(datax, datay, label='VSS', parent='y_axis',
                                 tag='mem')
 
@@ -179,20 +159,31 @@ class MainWindow:
             dpg.configure_item('modal_id', show=True)
             return
 
+        self.update_quote()
+        self.update_ui_before_run()
+        self.start_simulate_process()
+        self.start_monitor_thread()
+
+    def update_quote(self):
         if platform.system() == 'Linux':
             quote = get_quote()
             dpg.set_value('quote_text', quote)
 
-        dpg.configure_item(sender, enabled=False)
+    def update_ui_before_run(self):
+        dpg.configure_item('run_button', enabled=False)
         dpg.configure_item('ind', show=True)
         dpg.set_value('mem', [[], []])
-        mq = multiprocessing.Queue()
-        
-        proc = multiprocessing.Process(target=report_mem, args=(mq,))
-        proc.daemon = True
-        proc.start()
+        dpg.configure_item('run_status', color=(255,255,0))
+        dpg.set_value('run_status', 'Started')
 
-        monitor_thread = threading.Thread(target=monitor_mem, args=(mq,))
+    def start_simulate_process(self):
+        count = dpg.get_value('count')
+        interval = dpg.get_value('interval')
+        proc = ReportMemoryProcess(self.queue, count, interval)
+        proc.start()
+    
+    def start_monitor_thread(self):
+        monitor_thread = threading.Thread(target=self.my_monitor_mem)
         monitor_thread.start()
 
     def select_all_callback(self, sender):
@@ -204,6 +195,42 @@ class MainWindow:
         ts_tags = ('t1', 't2', 't3', 't4', 't5')
         for ts in ts_tags:
             dpg.set_value(ts, False)
+    
+    def my_monitor_mem(self):
+        datax = []
+        datay = []
+        p  = 0
+        dpg.set_value('progress', p)
+        while True:
+            x = self.queue.get()
+            if x == 'done':
+                print('done')
+                break
+            datax.append(x[0])
+            datay.append(x[1])
+            dpg.set_value('mem', [datax, datay])
+            p += 0.1
+            dpg.set_value('progress', p)
+        self.update_ui_after_run()
+
+    def update_ui_after_run(self):
+        self.update_run_status()
+        dpg.set_value('test_summary', get_fake_markdown())
+        dpg.set_value('test_xml', get_fake_text())
+        dpg.set_value('test_valgrind', get_fake_text())
+        dpg.configure_item('ind', show=False)
+        dpg.configure_item('run_button', enabled=True)
+
+    def update_run_status(self):
+        x = random.random()
+        if x > 0.5 :
+            color = (0, 255, 0)
+            text = 'Sucess'
+        else:
+            color = (255, 0, 0)
+            text = 'Failed'
+        dpg.configure_item('run_status', color=color)
+        dpg.set_value('run_status', text)
 
 
 class App:
@@ -223,6 +250,11 @@ class App:
 
     def __del__(self):
         dpg.destroy_context()
+
+
+def main():
+    app = App()
+    app.run()
 
 
 if __name__ == '__main__':
