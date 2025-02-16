a92b9549644e9d567b8043428fd39ab112327515
zliu <zgliu2@gmail.com>
2025-02-15 17:04:23 -0800

    update

--- test2.py
+++ test2.py
@@ -1,17 +1,20 @@
 import dearpygui.dearpygui as dpg
+import dearpygui.demo as demo
 
 def save_callback():
     print("Save clicked")
 
 dpg.create_context()
-dpg.create_viewport()
+dpg.create_viewport(title="Test", width=600, height=300)
 dpg.setup_dearpygui()
 
 with dpg.window(label="Main Window"):
     dpg.add_text("Hello")
     dpg.add_button(label="Save", callback=save_callback)
-    dpg.add_input_text(label="Input")
-    dpg.add_slider_float(label="float")
+    dpg.add_input_text(label="Input", default_value="Quick brown fox")
+    dpg.add_slider_float(label="float", default_value=0.273, max_value=1.0)
+
+demo.show_demo()
 
 dpg.show_viewport()
 dpg.start_dearpygui()
