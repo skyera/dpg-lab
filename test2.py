51f410578beb871ff2cf3e0b818aa8e3a403c07a
zliu <zgliu2@gmail.com>
2025-02-17 11:36:37 -0800

    update, add image

--- test2.py
+++ test2.py
@@ -2,21 +2,33 @@ import dearpygui.dearpygui as dpg
 import dearpygui.demo as demo
 
 
-def save_callback():
-    print("Save clicked")
+def open_callback(sender, app_dat, user_data):
+    print("Sender: ", sender)
+    print("App Data: ", app_dat)
+    print("User Data: ", user_data)
 
 
 dpg.create_context()
 
+with dpg.file_dialog(
+    directory_selector=False,
+    show=False,
+    callback=open_callback,
+    id="file_dialog",
+    width=600,
+    height=400,
+):
+    dpg.add_file_extension(".*")
+    dpg.add_file_extension("", color=(150, 255, 150, 255))
+    dpg.add_file_extension("*.png", color=(150, 150, 255, 255), custom_text="PNG")
+
 with dpg.window(label="Main Window", tag="Primary Window"):
-    dpg.add_text("Hello")
-    dpg.add_button(label="Save", callback=save_callback)
+    dpg.add_text("Choose Image")
+    dpg.add_button(label="Open", callback=lambda: dpg.show_item("file_dialog"))
     dpg.add_input_text(label="Input", default_value="Quick brown fox")
     dpg.add_slider_float(label="float", default_value=0.273, max_value=1.0)
 
-dpg.create_viewport(title="Test", width=600, height=300)
-demo.show_demo()
-
+dpg.create_viewport(title="Test", width=600, height=400)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.set_primary_window("Primary Window", True)
